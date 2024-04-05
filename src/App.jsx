bfecfa67ded3712ad99a464aecd71a478bf46735
Makotosoul <minhthien.ng4612@gmail.com>
2024-04-05 15:45:33 +0300

    router with MUI menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,67 @@
-import { Link, Outlet } from "react-router-dom";
-import './App.css';
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import React from "react";
+import "./index.css";
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
+
+import Home from "./components/Home.jsx";
+import Contact from "./components/Contact.jsx";
+import About from "./components/About.jsx";
+import Error from "./components/Error.jsx";
+import "./App.css";
 function App() {
-  return (
-    <div className="App">
-      <nav>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/about"}>About</Link>
-        <Link to={"/contact"}>Contact</Link>
-      </nav>
-      <Outlet />
-    </div>
-  );
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+	return (
+		<BrowserRouter>
+		{/*copy and paste from the mui document *from here**/}
+			<Button
+				id="basic-button"
+				aria-controls={open ? "basic-menu" : undefined}
+				aria-haspopup="true"
+				aria-expanded={open ? "true" : undefined}
+				onClick={handleClick}
+			>
+		<MenuIcon/>
+			</Button>
+			<Menu
+				id="basic-menu"
+				anchorEl={anchorEl}
+				open={open}
+				onClose={handleClose}
+				MenuListProps={{
+					"aria-labelledby": "basic-button",
+				}}
+			>
+		{/**to here**/}
+		{/*adding component link and the /endpoint*/}
+				<MenuItem onClick={handleClose} component={Link} to="/">
+					Home
+				</MenuItem>
+				<MenuItem onClick={handleClose} component={Link} to="/contact">
+					Contact
+				</MenuItem>
+				<MenuItem onClick={handleClose} component={Link} to="/about">
+					About
+				</MenuItem>
+			</Menu>
+		{/*making routing path for the link*/}
+			<Routes>
+				<Route path="/" exact Component={Home} />
+				<Route path="/contact" exact Component={Contact} />
+				<Route path="/about" exact Component={About} />
+				<Route Component={Error} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,10 @@
-import { createBrowserRouter, RouterProvider} from 'react-router-dom';
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import App from './App.jsx'
-import Home from './components/Home.jsx'
-import Contact from './components/Contact.jsx'
-import About from './components/About.jsx'
-
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-		children: [
-			{
-				element:<Home />,
-				index: true
-			},
-			{
-				path: "about",
-				element: <About />
-			},
-			{
-				path: "contact",
-				element: <Contact />
-			},
-		]
-	}
-]);
-
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<App />
 	</React.StrictMode>,
 );
